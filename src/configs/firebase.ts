7764cb301048cf053c7813d8a255aab46b4801d9
DudaevDenii <125597264+DudaevDenii@users.noreply.github.com>
2023-11-12 19:40:19 -0600

    Update

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -3,12 +3,7 @@ import { getFirestore, collection } from "firebase/firestore";
 import { useFirebaseAuth } from "vuefire";
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAvJDC-97d_Ane5FUHA07N_Lv6Z7UGlQFo",
-  authDomain: "run-chat-d574e.firebaseapp.com",
-  projectId: "run-chat-d574e",
-  storageBucket: "run-chat-d574e.appspot.com",
-  messagingSenderId: "121366995570",
-  appId: "1:121366995570:web:1618090b2b6ad482d118eb",
+//Your Api key
 });
 
 const db = getFirestore(firebaseApp);
